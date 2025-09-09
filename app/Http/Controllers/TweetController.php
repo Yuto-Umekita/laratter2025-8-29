29bc15f0b37eaf77dfe7eaa4a4e73fc1d12178c9
Yuto-Umekita <s23c1080@bene.fit.ac.jp>
2025-09-09 19:21:06 +0900

    処理

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -55,7 +55,8 @@ class TweetController extends Controller
      */
     public function edit(Tweet $tweet)
     {
-        //
+    return view('tweets.edit', compact('tweet'));
+        
     }
 
     /**
@@ -63,7 +64,13 @@ class TweetController extends Controller
      */
     public function update(Request $request, Tweet $tweet)
     {
-        //
+        $request->validate([
+      'tweet' => 'required|max:255',
+    ]);
+
+    $tweet->update($request->only('tweet'));
+
+    return redirect()->route('tweets.show', $tweet);
     }
 
     /**
@@ -71,6 +78,8 @@ class TweetController extends Controller
      */
     public function destroy(Tweet $tweet)
     {
-        //
+    $tweet->delete();
+
+    return redirect()->route('tweets.index');
     }
 }
